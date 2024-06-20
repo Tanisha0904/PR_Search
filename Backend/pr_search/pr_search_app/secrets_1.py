80172f18192753ce49fc8fff8791978b8dafffad
Tanisha0904 <tanishakumthekar9@gmail.com>
2024-06-20 12:59:01 +0530

    gpt working

--- Backend/pr_search/pr_search_app/secrets_1.py
+++
@@ -1,6 +0,0 @@
-GITHUB_TOKEN = "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"
-# OPENAI_API_KEY = "sk-proj-IJNuRvrsuKPB555xccPGT3BlbkFJ2nnqmVXyJHxUgNfAcdq4"
-# OPENAI_API_KEY = "sk-proj-VxsPrZws8maUAGloF2xMT3BlbkFJ1sxsyykkwAg9IJ0NLK0r" #My key1 - exceeded current quota
-# OPENAI_API_KEY = "sk-proj-UyAvQGJ3TOVhzNqS0Uw9T3BlbkFJ09Dt3taIVGIVnX7bdvh6" #My key2
-OPENAI_API_KEY = "sk-proj-Hyt1pcZtIHxRocpnTX22T3BlbkFJkDArIyTc4ZTbNXfB1wp2" #My key3
-
